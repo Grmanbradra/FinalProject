d8f9ff45708f2d93b0de2664fde39d73215c7bbb
DESKTOP-HHOATM6\Gr_ma <rn.manbradra@gmail.com>
2017-12-07 21:51:17 +0700

    no message

---
+++ final/login/add_user.php
@@ -0,0 +1,39 @@
+<?php
+// Include config file
+require_once 'config.php';
+
+if ($_POST){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $gender = $_POST["gender"];
+    $status = 0;
+
+//    print_r($username);
+    try{
+        $sql = "INSERT INTO user_login
+						  (user_name,user_pass,
+						   user_email,user_phone,user_status,
+						   user_gender)
+					       VALUES(:user_name,:user_pass,:user_email,:user_phone,:user_status,:user_gender)";
+        $db = $pdo;
+        $stmt = $db->prepare($sql);
+//                $stmt->bindParam(':id', "");
+        $stmt->bindParam(':user_name', $username);
+        $stmt->bindParam(':user_pass', $password);
+        $stmt->bindParam(':user_email', $email);
+        $stmt->bindParam(':user_phone', $phone);
+        $stmt->bindParam(':user_status', $status);
+        $stmt->bindParam(':user_gender', $gender);
+        $max = "";
+
+        if ($stmt->execute()) {
+            header('location:../login.php');
+            echo "Add complete";
+        }
+    }catch (PDOException $ex){
+        echo 'ERROR -> ' . $ex->getMessage();
+    }
+}
+?>
--- final/login/register.php
+++ final/login/register.php
@@ -1,8 +1,3 @@
-<?php
-// Include config file
-require_once 'config.php';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,85 +8,83 @@ require_once 'config.php';
         body{ font: 14px sans-serif; }
         .wrapper{ width: 350px; padding: 20px; }
     </style>
+    <link rel="stylesheet" href="../css/from_register.css">
 </head>
 <body>
-<div class="wrapper">
-    <h2>Sign Up</h2>
-    <p>Please fill this form to create an account.</p>
-    <form action="" method="post">
-        <div class="form-group ">
-            <label>Username:<sup>*</sup></label>
-            <input type="text" name="username"class="form-control">
-            <span class="help-block"></span>
-        </div>
-        <div class="form-group">
-            <label>Password:<sup>*</sup></label>
-            <input type="password" name="password" class="form-control">
-            <span class="help-block"></span>
-        </div>
-        <div class="form-group">
-            <label>Confirm Password:<sup>*</sup></label>
-            <input type="password" name="confirm_password" class="form-control">
-            <span class="help-block"></span>
-        </div>
-        <div class="form-group">
-            <label>Email:<sup>*</sup></label>
-            <input type="text" name="email" class="form-control">
-            <span class="help-block"></span>
-        </div>
-        <div class="form-group">
-            <label>Phone:<sup>*</sup></label>
-            <input type="text" name="phone" class="form-control">
-            <span class="help-block"></span>
-        </div>
-        <div class="form-group">
-            <label>Gender:<sup>*</sup></label>
-            <input type="radio" name="gender" value="Male"> Male
-            <input type="radio" name="gender" value="Female"> Female
-            <span class="help-block"></span>
-        </div>
-        <div class="form-group">
-            <input type="submit" class="btn btn-primary" value="Submit">
-            <input type="reset" class="btn btn-default" value="Reset">
-        </div>
-        <p>Already have an account? <a href="../login.php">Login here</a>.</p>
-    </form>
-    <?php
-        if ($_POST){
-            $username = $_POST["username"];
-            $password = $_POST["password"];
-            $email = $_POST["email"];
-            $phone = $_POST["phone"];
-            $gender = $_POST["gender"];
-            $status = 0;
 
-            print_r($username);
-            try{
-                $sql = "INSERT INTO user_login
-						  (user_name,user_pass,
-						   user_email,user_phone,user_status,
-						   user_gender)
-					       VALUES(:user_name,:user_pass,:user_email,:user_phone,:user_status,:user_gender)";
-                $db = $pdo;
-                $stmt = $db->prepare($sql);
-//                $stmt->bindParam(':id', "");
-                $stmt->bindParam(':user_name', $username);
-                $stmt->bindParam(':user_pass', $password);
-                $stmt->bindParam(':user_email', $email);
-                $stmt->bindParam(':user_phone', $phone);
-                $stmt->bindParam(':user_status', $status);
-                $stmt->bindParam(':user_gender', $gender);
-                $stmt->execute();
-                $max = "";
+<div class="container">
+    <div class="row main">
+        <div class="panel-heading">
+            <div class="panel-title text-center">
+                <h1 class="title">Sing Up</h1>
+                <hr />
+            </div>
+        </div>
+        <div class="main-login main-center">
+            <form method="post" action="add_user.php">
 
-                if ($stmt->execute()) {
-                    echo "Add complete";
-                }
-            }catch (PDOException $ex){
-                echo 'ERROR -> ' . $ex->getMessage();
-            }
-        }
-    ?>
+                <div class="form-group">
+                    <label for="name" class="cols-sm-2 control-label">Your Username *</label>
+                    <div class="cols-sm-10">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i>Username:</i></span>
+                            <input type="text" class="form-control" name="username" placeholder="Enter your username"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="password" class="cols-sm-2 control-label">Password*</label>
+                    <div class="cols-sm-10">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i>Password:</i></span>
+                            <input type="password" class="form-control" name="password"  placeholder="Enter your Password"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="email" class="cols-sm-2 control-label">Your Email</label>
+                    <div class="cols-sm-10">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i>Email:</i></span>
+                            <input type="text" class="form-control" name="email"  placeholder="Enter your Email"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="username" class="cols-sm-2 control-label">Phone</label>
+                    <div class="cols-sm-10">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i>Phone:</i></span>
+                            <input type="text" class="form-control" name="phone" placeholder="Enter your Phone"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="password" class="cols-sm-2 control-label">Gender</label>
+                    <div class="cols-sm-10">
+                        <div class="input-group">
+                            <input type="radio" name="gender" value="Male"> Male
+                            <input type="radio" name="gender" value="Female"> Female
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group ">
+                    <input type="submit" class="btn btn-primary btn-lg btn-block login-button" value="Register">
+                </div>
+                <div class="login-register">
+                    <a href="../login.php">Login</a>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!--<script type="text/javascript" src="../vendor/bootstrap/js/bootstrap.js"></script>-->
 </div>
 </body>
 </html>
