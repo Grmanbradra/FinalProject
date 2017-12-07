418a25378ad6fb189ddc4b24cbc42ecbe0daa1b6
DESKTOP-HHOATM6\Gr_ma <rn.manbradra@gmail.com>
2017-12-08 06:32:39 +0700

    Detail User Guide

--- final/process_page/add_file/use-details.php
+++ final/process_page/add_file/use-details.php
@@ -4,33 +4,33 @@
 
         <div class="panel panel-info">
             <div class="panel-heading">
-                <h3 class="panel-title">Panel info</h3>
+                <h3 class="panel-title">ขั้นตอนที่ 1 </h3>
             </div>
-            <div class="panel-body">Panel content</div>
+            <div class="panel-body">คลิกเมนูส่วนผู้ใช้งาน => เพิ่มไฟล์ => เพิ่มข้อมูล</div>
         </div>
 
 
         <div class="panel panel-info">
             <div class="panel-heading">
-                <h3 class="panel-title">Panel info</h3>
+                <h3 class="panel-title">ขั้นตอนที่ 2</h3>
             </div>
-            <div class="panel-body">Panel content</div>
+            <div class="panel-body">กรอกรายละเอียดของ ชื่อหนังสือ ชื่อผู้แต่ง ปีที่พิมพ์</div>
         </div>
 
 
         <div class="panel panel-info">
             <div class="panel-heading">
-                <h3 class="panel-title">Panel info</h3>
+                <h3 class="panel-title">ขั้นตอนที่ 3</h3>
             </div>
-            <div class="panel-body">Panel content</div>
+            <div class="panel-body">เลือกไฟล์ พีดีเอฟ(PDF) ที่ต้องการอัพโหลด</div>
         </div>
 
 
         <div class="panel panel-info">
             <div class="panel-heading">
-                <h3 class="panel-title">Panel info</h3>
+                <h3 class="panel-title">ข้อควรระวัง</h3>
             </div>
-            <div class="panel-body">Panel content</div>
+            <div class="panel-body">ควรกรอกรายละเอียดให้ครบ(ชื่อหนังสือควรเป็นภาษาอังกฤษ) และเลือกไฟล์ให้ตรงกับชนิดที่ระบบ ต้องการเพื่อไม่ให้ระบบเกิดความผิดพลาด</div>
         </div>
 
     </div>
