7c8bf8a8dd283a506f9ff21fac428f1d22b9d39a
Sathaporn Sunthornpan <dd.stitch55@gmail.com>
2017-12-08 00:44:10 +0700

    fix form_add.

--- final/process_page/add_file/form_add.php
+++ final/process_page/add_file/form_add.php
@@ -1,7 +1,39 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: tisstp
- * Date: 7/12/2017 AD
- * Time: 23:24
- */
+<div class="card">
+    <h3 class="card-title">กรุณากรอกรายละเอียดของหนังสือ</h3>
+    <form action="<?= url('add_file.php?action=insert');?>" method="post" enctype="multipart/form-data">
+        <table class="table table-bordered">
+            <tbody>
+            <tr>
+                <td width="250">ชื่อหนังสือ</td>
+                <td>
+                    <input type="text" name="book_name" style="width: 100%">
+                </td>
+            </tr>
+            <tr>
+                <td width="250">ชื่อผู้แต่ง</td>
+                <td>
+                    <input type="text" name="book_author" style="width: 100%">
+
+                </td>
+            </tr>
+            <tr>
+                <td width="250">ปีที่พิมพ์</td>
+                <td>
+                    <input type="text" name="book_year" style="width: 100%">
+                </td>
+            </tr>
+            <tr>
+                <td width="250">อัพโหลดไฟล์</td>
+                <td>
+                    <input type="file" name="book_file" style="width: 100%">
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <p class="bs-component"><button type="submit" name="submit" class="btn btn-default btn-lg btn-block" href="#">บันทึก</button></p>
+    </form>
+    <br>
+
+    <a href="<?= url('add_file.php');?>" class="btn btn-success">ย้อนกลับ</a>
+</div>
+
