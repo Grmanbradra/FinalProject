66ef22925ce87630cf289b44cb8016f448d629ba
Sathaporn Sunthornpan <dd.stitch55@gmail.com>
2017-12-08 13:20:05 +0700

    fix : delete file_data ref_fiel & remove file pdf

--- final/process_page/folders/delete.php
+++ final/process_page/folders/delete.php
@@ -2,12 +2,42 @@
 
 try {
 
+    // select delete file
+    $sql = "SELECT path_file FROM file_data WHERE id_file=:id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindValue(':id', $id);
+    $check = false;
+    if($stmt->execute()) {
+        $data = $stmt->fetch();
+
+        $directory = dirname(dirname(__DIR__)) . '/'. $data['path_file'];
+
+        echo '<pre>';
+        echo $directory . "<br>";
+
+        if(file_exists($directory)) {
+            echo 'file_exists';
+            if(unlink($directory)) {
+                echo 'remove file success';
+                $check = true;
+            }
+        } else {
+            $check = true;
+        }
+
+    }
+
+
     // sql to delete a record
-    $sql = "DELETE FROM categories_water WHERE water_id=:id";
+    $sql = "DELETE FROM file_data WHERE id_file=:id";
     $stmt = $conn->prepare($sql);
     $stmt->bindValue(':id', $id);
 
-    if($stmt->execute()) {
+    $sql = "DELETE FROM ref_file WHERE id_file=:id";
+    $stmt2 = $conn->prepare($sql);
+    $stmt2->bindValue(':id', $id);
+
+    if($stmt2->execute() && $check && $stmt->execute()) {
         // success
         echo "<script>swal('Success!', 'Record deleted successfully', 'success')</script>";
 //            $_SESSION['notification']['success'] = "New record created successfully";
